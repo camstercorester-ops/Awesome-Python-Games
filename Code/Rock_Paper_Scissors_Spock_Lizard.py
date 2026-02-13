07d712d478016190f1b8697d71547b04969c1d75
camstercorester-ops <camstercorester@gmail.com>
2026-02-13 12:20:23 +0000

    style: remove extra blank line in enum class definition

--- Code/Rock_Paper_Scissors_Spock_Lizard.py
+++ Code/Rock_Paper_Scissors_Spock_Lizard.py
@@ -13,7 +13,6 @@ from typing import Final, Tuple
 # Domain model
 # ------------------------------------------------------------------
 
-
 @unique
 class Move(IntEnum):
     """Strongly-typed moves with built-in conversion."""
